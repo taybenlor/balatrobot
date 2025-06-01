45f0b2854470de03731e814ac3ccdf1742c3e3e8
Rhys Walsh-Tindall <rhys_walsh@hotmail.com>
2025-06-01 21:52:17 +1000

    Added logic for determining if backup hands can be played when there are 0 discards

--- balatro_objects.py
+++ balatro_objects.py
@@ -1,5 +1,6 @@
 from enum import IntEnum
 from collections import Counter
+from itertools import combinations
 
 
 class Hand:
@@ -10,43 +11,76 @@ class Hand:
         return f'The hand contains {len(self.cards)} cards, with values {[card.card_key for card in self.cards]}'
 
     def suit_check(self):
+        # I know I should just be using the Counter object, sue me!
         return dict(Counter(card.suit for card in self.cards))
     
     def value_check(self):
+        # I know I should just be using the Counter object, sue me again!
         return dict(Counter(card.value for card in self.cards))
 
     def get_cards_in_suit(self, suit=None):
         # will always return highest values first
         filtered_cards = [card for card in self.cards if card.suit == suit]
-        print("Filtered suit cards: {}".format(filtered_cards))
-        return sorted(filtered_cards, key=lambda card: card.get_card_ranking())
+        # print("Filtered suit cards: {}".format(filtered_cards))
+        return sorted(filtered_cards, key=lambda card: card.get_card_ranking(), reverse=True)
 
     def get_cards_with_value(self, value=None):
         filtered_cards = [card for card in self.cards if card.value == value]
-        print("Filtered value cards: {}".format(filtered_cards))
+        # print("Filtered value cards: {}".format(filtered_cards))
         # TODO: How do we sort based on the best value modification approach?
-        return sorted(filtered_cards, key=lambda card: card.get_card_ranking())
+        return sorted(filtered_cards, key=lambda card: card.get_card_ranking(), reverse=True)
+    
+    def get_most_common_cards(self, value_count={}):
+        # By calling this, we're assuming there is one card value that
+        # has a higher count compared to all others.
+        highest_card_frequency = max(value_count.values())
+        most_common_values = [value for value, count in value_count.items() if count == highest_card_frequency]
+        cards = []
+        print("Most Common Values: {}".format(most_common_values))
+        for value in most_common_values:
+            # TODO: How do we sort based on the best value modification approach?
+            cards.extend(self.get_cards_with_value(value=value))
+        return cards
 
     # Are hands playable (in order of their poker ranking)
     def get_royal_flush(self):
-        # TODO here.
+        ROYAL_VALUES = {'10', 'J', 'Q', 'K', 'A'}
+        for five_cards in combinations(self.cards, 5):
+            suits = {card.suit for card in five_cards}
+            values = {card.value for card in five_cards}
+            if len(suits) == 1 and values == ROYAL_VALUES:
+                # By golly, we've actually got one!
+                # Fun fact, the odds of getting a royal flush are 1 in 649,739 (with a standard 52 card deck)
+                print("I can play a Royal Flush!")
+                return [card.index for card in five_cards]
         return []
 
     def get_straight_flush(self):
         # TODO here.
+        # We'll have to combine approaches between royal flush and straight.
+        # print("I can play a Straight Flush!")
         return []
 
     def get_four_of_a_kind(self):
         # are there one or more groups of 4 or more same cards?
-        if any(count >= 4 for count in self.value_check().values()):
-            return []
+        value_count = self.value_check()
+        if any(count >= 4 for count in value_count.values()):
+            cards = self.get_most_common_cards(value_count=value_count)
+            print("I can play Four of a Kind!")
+            return [card.index for card in cards]
         return []
 
     def get_full_house(self):
         # are there one or more groups of 3 or more cards?
         # are there one or more groups of 2 or more cards? (including the group from above)
-        if any(count >= 3 for count in self.value_check().values()) and sum(1 for count in self.value_check().values() if count >= 2) >= 2:
-            return []
+        value_count = self.value_check()
+        if any(count >= 3 for count in value_count.values()) and sum(1 for count in value_count.values() if count >= 2) >= 2:
+            first_group = self.get_most_common_cards(value_count=value_count)
+            value_count_copy = value_count.copy()
+            del value_count_copy[max(value_count, key=value_count.get)]
+            second_group = self.get_most_common_cards(value_count=value_count_copy)
+            print("I can play a Full House!")
+            return [card.index for card in first_group].extend([card.index for card in second_group])
         return []
 
     def get_flush(self):
@@ -56,30 +90,40 @@ class Hand:
             print("Most Common Suit: {}".format(most_common_suit))
             # TODO: What happens if we have two suits with the same number of cards?
             cards = self.get_cards_in_suit(suit=most_common_suit)
+            print("I can play a Flush!")
             return [card.index for card in cards[:5]]
         return []
 
     def get_straight(self):
-        # fuck, how do we do this, using ordering maybe?
-        # Big TODO here.
+        for five_cards in combinations(self.cards, 5):
+            values = sorted(card.get_card_ranking() for card in five_cards)
+            alt_values = [1 if v == 14 else v for v in values].sort()
+            if all(values[i] == values[i - 1] + 1 for i in range(1, 5)) or all(alt_values[i] == alt_values[i - 1] + 1 for i in range(1, 5)):
+                print("I can play a Straight!")
+                return [card.index for card in five_cards]
         return []
     
     def get_three_of_a_kind(self):
-        if any(count >= 3 for count in self.value_check().values()):
-            return []
+        value_count = self.value_check()
+        if any(count >= 3 for count in value_count.values()):
+            cards = self.get_most_common_cards(value_count=value_count)
+            print("I can play Three of a Kind!")
+            return [card.index for card in cards]
         return []
     
     def get_two_pair(self):
-        if sum(1 for count in self.value_check().values() if count >= 2) >= 2:
-            return []
+        value_count = self.value_check()
+        if sum(1 for count in value_count.values() if count >= 2) >= 2:
+            cards = self.get_most_common_cards(value_count=value_count)
+            print("I can play Two Pair!")
+            return [card.index for card in cards]
         return []
     
     def get_pair(self):
         value_count = self.value_check()
         if any(count >= 2 for count in value_count.values()):
-            most_common_value = max(value_count, key=value_count.get)
-            print("Most Common Value: {}".format(most_common_value))
-            cards = self.get_cards_with_value(value=most_common_value)
+            cards = self.get_most_common_cards(value_count=value_count)
+            print("I can play a pair!")
             return [card.index for card in cards]
         return []
 

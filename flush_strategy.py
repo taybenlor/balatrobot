91c0faf6f2e8fa89d54c3ef5e1b9b2a8a7de4d03
Rhys Walsh-Tindall <rhys_walsh@hotmail.com>
2025-06-08 18:48:19 +1000

    Remove old flush bot file and add new strategy

---
+++ flush_strategy.py
@@ -0,0 +1,238 @@
+import time
+import argparse
+
+from utils import delete_game_cache
+from bot import Bot, Actions
+from balatro_objects import Hand, Card
+
+
+def skip_or_select_blind(self, G):
+    if (
+        G["ante"]["blinds"]["ondeck"] == "Small"
+        or G["ante"]["blinds"]["ondeck"] == "Big"
+    ):
+        return [Actions.SKIP_BLIND]
+    else:
+        return [Actions.SELECT_BLIND]
+
+
+def select_cards_from_hand(self, G):
+    # G["hand"] is a list of cards in the hand
+
+    # Example of playing the first card in the hand
+    # return [Actions.PLAY_HAND, [1]]
+
+    # Example of discarding the first card in the hand
+    # return [Actions.DISCARD_HAND, [1]]
+
+    # Flush Strategy
+    # - Check hand for number of each suit
+    # - Based on suit number, is there a flush we can play?
+    #     - If yes, play the flush
+    #     - If no, continue 
+    # - Check deck for number of each suit left available
+    # - Discard lowest frequency suits (up to 5 cards)
+
+    # Note:
+    # Take into account boss level (are we debuffing a particular suit)
+
+    if "hands_played" not in self.state:
+        self.state["hands_played"] = 0
+
+    cards = []
+    for index, card in enumerate(G["hand"]):
+        cards.append(
+            Card(
+                suit=card['suit'],
+                label=card['label'],
+                value=card['value'],
+                name=card['name'],
+                debuff=card['debuff'],
+                card_key=card['card_key'],
+                index=index + 1
+            )
+        )
+    hand = Hand(
+        cards=cards
+    )
+
+    # What's the actual hand we're going to play?
+    play_hand = None
+
+    if hand.get_flush():
+        # We can play a flush!
+        self.state["hands_played"] += 1
+        play_hand = hand.get_flush()
+        print("Going to play hand: {}".format(play_hand))
+        return [
+            Actions.PLAY_HAND,
+            play_hand
+        ]
+    
+    # Okay, we can't play a flush, maybe we should try to discard instead!
+    # Let's just firstly figure out what we should discard first.
+    suit_count = hand.suit_check()
+    discard_hand = []
+    # We don't want to include the flush suit in our discard, as this goes against the purpose of discarding!
+    while len(discard_hand) < 5 and len(suit_count.keys()) > 1:
+        least_common_suit = min(suit_count, key=suit_count.get)
+        suit_count.pop(least_common_suit)
+        for card in hand.cards:
+            if card.suit == least_common_suit and len(discard_hand) < 5:
+                discard_hand.append(card.index)
+    # If we have discards, let's go with this strategy.
+    if G["current_round"]["discards_left"] > 0:
+        print("Going to discard hand: {}".format(discard_hand))
+        return [
+            Actions.DISCARD_HAND,
+            discard_hand
+        ]
+
+    # We've really screwed the pooch here.
+    # We don't have a valid flush to play, but we don't have any discards left either.
+    # What other hands could we play?
+    play_hand = (
+        hand.get_royal_flush() if hand.get_royal_flush() else
+        hand.get_straight_flush() if hand.get_straight_flush() else
+        hand.get_four_of_a_kind() if hand.get_four_of_a_kind() else
+        hand.get_full_house() if hand.get_full_house() else
+        hand.get_three_of_a_kind() if hand.get_three_of_a_kind() else
+        hand.get_two_pair() if hand.get_two_pair() else
+        hand.get_pair() if hand.get_pair() else
+        []
+    )
+
+    # We're going to be playing a hand now whether we like it or not.
+
+    self.state["hands_played"] += 1
+    if play_hand:
+        # We've found an alternative hand to play
+        print("Playing an alternative hand: {}".format(play_hand))
+        return [Actions.PLAY_HAND, play_hand]
+    else:
+        # We legitimately 
+        print("No discards available. Going to play discardable hand: {}".format(discard_hand))
+        return [Actions.PLAY_HAND, discard_hand]
+
+
+def select_shop_action(self, G):
+    if "num_shops" not in self.state:
+        self.state["num_shops"] = 0
+
+    self.state["num_shops"] += 1
+
+    # Shop prioritization order:
+    # 1 - Jokers
+    # 2 - Boosters
+    # 3 - Vouchers
+    # Except in the case that the "Clearance Sale" (25% off) voucher is available - we should buy this, provided we have a bit of money to spend afterwards
+
+    # look at G["dollars"] - that's our current wallet
+    current_dollars = G["dollars"]
+
+    voucher_names = [voucher["label"] for voucher in G["shop"]["vouchers"]]
+    if "Clearance Sale" in voucher_names and current_dollars >= 15:
+        # this will make things cheaper moving forward
+        idx = voucher_names.index("Clearance Sale")
+        return [Actions.BUY_VOUCHER, [idx + 1]]
+
+    multi_based_jokers = [
+        'Cavendish','Gros Michel','Misprint','Joker','Greedy Joker','Lusty Joker','Wrathful Joker','Gluttonous Joker','Droll Joker','Photograph','Popcorn','Smiley Face','Joker Stencil','Loyalty Card','Fibonacci','Card Sharp','Ramen','Baseball Card','Raised Fist', 'Abstract Joker', 'Even Steven', 'Supernova',
+    ]
+    chip_based_jokers = [
+        'Crafty Joker', 'Hiker', 'Banner', 'Odd Todd', 'Scary Face', 'Ice Cream', 'Blue Joker'
+    ]
+    other_jokers = [
+        'Four Fingers', 'Turtle Bean', 'Mr. Bones'
+    ]
+
+    # let's check jokers
+    for index, joker in enumerate(G["shop"]["cards"]):
+        cost = joker["cost"]
+        label = joker["label"]
+        affordable = cost < current_dollars
+        if label in multi_based_jokers and affordable:
+            return [Actions.BUY_CARD, [index + 1]]
+        elif label in chip_based_jokers and affordable:
+            return [Actions.BUY_CARD, [index + 1]]
+        elif label in other_jokers and affordable:
+            return [Actions.BUY_CARD, [index + 1]]
+
+    # let's check boosters
+    # TODO: Figure this out once we've implemented select_booster_action()
+
+    # let's check other vouchers
+    priority_vouchers = []
+    for index, voucher in enumerate(G["shop"]["vouchers"]):
+        cost = voucher["cost"]
+        label = voucher["label"]
+        affordable = cost < current_dollars
+        if label in priority_vouchers and affordable:
+            return [Actions.BUY_VOUCHER, [index + 1]]
+
+    # Nothing else left to do!
+    return [Actions.END_SHOP]
+
+
+def select_booster_action(self, G):
+    print("Choosing to Skip Booster Pack!")
+    print("Here is G: {}".format(G))
+    return [Actions.SKIP_BOOSTER_PACK]
+
+
+def sell_jokers(self, G):
+    if len(G["jokers"]) > 1:
+        return [Actions.SELL_JOKER, [2]]
+
+    return [Actions.SELL_JOKER, []]
+
+
+def rearrange_jokers(self, G):
+    return [Actions.REARRANGE_JOKERS, []]
+
+
+def use_or_sell_consumables(self, G):
+    return [Actions.USE_CONSUMABLE, []]
+
+
+def rearrange_consumables(self, G):
+    return [Actions.REARRANGE_CONSUMABLES, []]
+
+
+def rearrange_hand(self, G):
+    return [Actions.REARRANGE_HAND, []]
+
+
+if __name__ == "__main__":
+    # do we want to delete all our gamestate_cache files and start with a fresh state?
+    parser = argparse.ArgumentParser(description="Game cache management script")
+    parser.add_argument('--delete-game-cache', action='store_true', help='Delete all .json files in the gamestate_cache directory')
+    args = parser.parse_args()
+
+    if args.delete_game_cache:
+        delete_game_cache()
+
+    # creating balatro bot
+    mybot = Bot(
+        deck="Blue Deck",
+        stake=1,
+        seed=None,
+        challenge=None,
+        bot_port=12345
+    )
+
+    mybot.skip_or_select_blind = skip_or_select_blind
+    mybot.select_cards_from_hand = select_cards_from_hand
+    mybot.select_shop_action = select_shop_action
+    mybot.select_booster_action = select_booster_action
+    mybot.sell_jokers = sell_jokers
+    mybot.rearrange_jokers = rearrange_jokers
+    mybot.use_or_sell_consumables = use_or_sell_consumables
+    mybot.rearrange_consumables = rearrange_consumables
+    mybot.rearrange_hand = rearrange_hand
+
+    mybot.start_balatro_instance()
+    time.sleep(20)
+
+    mybot.run_step()
+    mybot.run()
